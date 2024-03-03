cf397dbeff78156fa9c7306793bc9d8b1fd27a00
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-03-03 21:49:17 +0200

    qqqq

--- djweather/weathermanager/views.py
+++ djweather/weathermanager/views.py
@@ -3,7 +3,6 @@ import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.conf import settings
 
 from icecream import ic
 
@@ -11,8 +10,6 @@ from core.responses.exceptions.exceptions import ApiException
 from core.responses.successes.successes import ApiSuccessResponse
 from .serializers import CityWeatherSerializer
 from .services.TomorrowIOService import TomorrowIOService, WeatherService
-
-API_KEY = getattr(settings, 'TOMORROW_IO_API_KEY', '1111111111')
 
 
 class CurrentWeatherView(APIView):
